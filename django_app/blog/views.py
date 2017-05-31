8417ffd6e381102872e55a59dfc1470d9544f416
HJ <tugratias@gmail.com>
2017-05-31 18:05:52 +0900

    add Post

--- django_app/blog/views.py
+++ django_app/blog/views.py
@@ -1,6 +1,12 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 from blog.models import Post
+from blog.forms import PostCreationForm
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 # Create your views here.
 
 def main_view(request):
@@ -14,14 +20,35 @@ def main_view(request):
         'posts':post
     }
 
-    return render(request, 'base/base.html', context)
+    return render(request, 'post/post-list.html', context)
 
 # def main_view2(request):
 #     return HttpResponse('메인뷰투투투투 음아')
 
 def post_add_view(request):
+    if request.method == 'GET':
+        form = PostCreationForm()
+
+        context = {
+            'forms':form
+        }
+        return render(request, 'post/post-add.html', context)
+
+    elif request.method == 'POST':
+        form = PostCreationForm(request.POST)
+
+
+        if form.is_valid():
+            author = User.objects.first()
+            title = form.cleaned_data['title']
+            text = form.cleaned_data['text']
+
+            Post.objects.create (
+                author = author,
+                title = title,
+                text = text,
+            )
+        return redirect('post_main')
+
     # 1. post_add_view(request) 함수를 만든다
     # 2. url 이름은 postadd 잘 연결 되었는지 확인한다
-
-
-
