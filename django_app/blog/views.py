248b322a42c01e46493051de03840dc6f75e25e0
HJ <tugratias@gmail.com>
2017-05-31 14:09:31 +0900

    post 모델 생성 초기 url 생성

---
+++ django_app/blog/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+
+# Create your views here.
+
+def main_view(request):
+    return HttpResponse('음 아 빨리 음 아 음음')
+
+def main_view2(request):
+    return HttpResponse('메인뷰투투투투 음아')
+
