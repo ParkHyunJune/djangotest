2a613832a49a4425733a8f757b662448d8d7ae4c
HJ <tugratias@gmail.com>
2017-05-31 16:11:06 +0900

    add main_view

--- django_app/blog/views.py
+++ django_app/blog/views.py
@@ -1,10 +1,27 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
+from blog.models import Post
 # Create your views here.
 
 def main_view(request):
-    return HttpResponse('음 아 빨리 음 아 음음')
 
-def main_view2(request):
-    return HttpResponse('메인뷰투투투투 음아')
+    post = Post.objects.all()
+
+    # for post in posts:
+    #     print(post.title)
+
+    context = {
+        'posts':post
+    }
+
+    return render(request, 'base/base.html', context)
+
+# def main_view2(request):
+#     return HttpResponse('메인뷰투투투투 음아')
+
+def post_add_view(request):
+    # 1. post_add_view(request) 함수를 만든다
+    # 2. url 이름은 postadd 잘 연결 되었는지 확인한다
+
+
 
--- django_app/mysite/settings.py
+++ django_app/mysite/settings.py
@@ -14,7 +14,11 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+ROOT_DIR = os.path.dirname(BASE_DIR)
+TEMPLATES_DIR = os.path.join(ROOT_DIR, 'templates')
 
+print('\n서버를 가동합니다')
+print('현재파일위치 :', os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -56,7 +60,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ TEMPLATES_DIR ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_DIR = os.path.join(ROOT_DIR, 'static')
+
+STATICFILES_DIR = {
+    STATIC_DIR,
+}
